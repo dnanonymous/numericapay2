1b61bad703e9ec48325594a0e7811ff05431a887
dnanonymous <adannbd@gmail.com>
2025-04-11 20:52:19 -0500

    .env backend

--- backend2/config/configDB.js
+++ backend2/config/configDB.js
@@ -1,5 +1,7 @@
 
-require('dotenv').config(); // Carga las variables de entorno
+const path = require('path');
+
+require('dotenv').config({path: path.resolve(__dirname, '../.env')});
 
 const mongoose = require('mongoose');
 
